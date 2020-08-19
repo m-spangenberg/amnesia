ed1afeb98f91cbe785c15a167559d3c6af661c47
Marthinus Spangenberg <mspangenberg.dev@gmail.com>
2020-08-20 00:27:29 +0200

    Change to chat and timestamp append method

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,9 @@ $(function () {
       return false;
    });
       socket.on('chat message', function(msg){
-         $('#messages').append($('<li>').text(msg));
+         const now = new Date();
+         const thismoment = now.toLocaleTimeString();
+         $("#chatroom").append("<div class=\"flex mx-4 mb-1\"><div><p class=\"text-sm mt-1 text-gray-600\">" + msg + "</p><p class=\"text-left text-xs text-gray-400 mt-1\">" + thismoment + "</p></div></div>");
    });
       socket.on('userCount', function(data){
          $('#userCounter').text(data);
