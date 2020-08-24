e2dbe5f8afe1779d1b5af49158ded42aed49d060
Marthinus Spangenberg <mspangenberg.dev@gmail.com>
2020-08-24 19:47:32 +0200

    Spring cleaning

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const namegen = require('./namegenerator');
-const fs = require('fs');
+const namegen = require('./src/namegenerator');
+// const fs = require('fs');
 
 let sessionUsers = [];
 
